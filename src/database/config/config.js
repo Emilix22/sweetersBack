fba716b4f765d5b8be5b4ab61d2a61c7da6dfc60
Emilix22 <torresdragon@hotmail.com>
2023-07-24 18:56:03 -0300

    agrego DB a conexion producción

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "id20666589_emilix",
+    "password": "Vadaju23$",
+    "database": "id20666589_sweetersdb",
+    "host": "localhost",
     "dialect": "mysql"
   }
 }
